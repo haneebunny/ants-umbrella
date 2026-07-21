27845d22a3bba663672fed4cc6a6163cf6c77467
haneebunny <re____@naver.com>
2026-07-21 10:26:23 +0900

    프로젝트 초기 세팅

--- backend/main.py
+++ backend/main.py
@@ -13,7 +13,7 @@ app.add_middleware(
 
 @app.get("/")
 def read_root():
-    return {"status": "ok", "message": "sesacdang API server is running!"}
+    return {"status": "ok", "message": "ants-umbrella API server is running!"}
 
 @app.get("/api/health")
 def health_check():
